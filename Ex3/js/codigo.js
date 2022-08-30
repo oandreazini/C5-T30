4a537bdab336d213faf998c35bb26877ab5010a4
oandreazini <oriol_as_92@hotmail.com>
2022-08-30 12:26:01 +0200

    Ex3 finished

---
+++ Ex3/js/codigo.js
@@ -0,0 +1,9 @@
+const meses = ["enereo", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto"
+    , "septiembre", "octubre", "noviembre", "deciembre"];
+
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i]);
+}
+
+
+
