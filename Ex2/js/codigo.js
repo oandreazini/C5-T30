4aedb8fccb5599a85344fbef79f4df044ca209e8
oandreazini <oriol_as_92@hotmail.com>
2022-08-30 11:42:37 +0200

    Ex2 finished

---
+++ Ex2/js/codigo.js
@@ -0,0 +1,7 @@
+//Imprimira por consola los dos siguientes mensajes, cuando sea llamado desde el archivo html
+console.log("Hola Mundo!");
+
+
+function alerta() {  
+    alert("Hola Mundo! Que facil es incluir" + "'comillas simples'"+ " y" + " comillas dobles" );
+} 
