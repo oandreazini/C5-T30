8af67fcc22488dbc67b07c887a5281c7604fbee6
oandreazini <oriol_as_92@hotmail.com>
2022-08-30 11:15:06 +0200

    Ex1 finished

--- Ex1/js/codigo.js
+++ Ex1/js/codigo.js
@@ -1,3 +1,4 @@
 //Imprimira por consola los dos siguientes mensajes, cuando sea llamado desde el archivo html
 console.log("Hola Mundo!");
-console.log("soy el primer script");
+
+document.getElementById("second").innerHTML = "Soy el primer script";
