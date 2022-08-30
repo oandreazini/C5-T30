5ea01d33461960b073c58860a513e0890a1d1524
oandreazini <oriol_as_92@hotmail.com>
2022-08-30 13:00:30 +0200

    Ex5 finished

---
+++ Ex5/js/codigo.js
@@ -0,0 +1,18 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero1 < numero2){
+    console.log("numero1 no es mayor que el numero2");
+}
+
+if(numero2 > 0){
+    console.log("numero2 es positivo");
+}
+
+if (numero1 == 5) {
+    console.log("numero1 es negativo o distinto a cero");
+}
+
+if (numero1 != 4) {
+    console.log("Incrementar en una unidad el valor numero1 no lo hace mayor o igual que el numero2");
+}
